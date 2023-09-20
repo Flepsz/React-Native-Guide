8c256f9d57edef83293d0665ae998dc7dc01d584
Felipe Pereira <luisfelipelfsp3@gmail.com>
2023-09-20 11:10:30 -0300

    :sparkles: feat: Template nativewind

---
+++ templates/typescript/nativewind/vanilla/App.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+import "./globals.css"
+
+export default function App() {
+  return (
+    <View className='bg-purple-700'>
+      <Text className='mt-20 text-white text-4xl'>Ready to use now :D</Text>
+      <Text className="mt-5 text-white text-2xl">React Native Typescript with Nativewind</Text>
+    </View>
+  );
+}
